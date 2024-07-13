4d4731a39477fbd436e28f1c3bf42965e94e27ea
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 00:01:09 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 const Pet = () => {
   const handleClickYes = () => {
@@ -10,52 +10,45 @@ const Pet = () => {
     alert("Give this link to ara plz :)");
   };
 
-  return React.createElement(
-    "div",
-    {
-      style: {
+  return (
+    <div
+      style={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center", // Memusatkan secara vertical dan horizontal
         minHeight: "100vh", // Mengisi seluruh tinggi viewport
-      },
-    },
-    React.createElement(
-      "div",
-      { style: { marginBottom: "20px" } },
-      React.createElement("img", {
-        src: "./testcat.png",
-        alt: "cat",
-        width: "400",
-        style: { alignSelf: "center" }, // Memusatkan gambar secara horizontal
-      })
-    ),
-    React.createElement(
-      "div",
-      {
-        style: {
+      }}
+    >
+      <div style={{ marginBottom: "20px" }}>
+        <img
+          src="./testcat.png"
+          alt="cat"
+          width="400"
+          style={{ alignSelf: "center" }} // Memusatkan gambar secara horizontal
+        />
+      </div>
+      <div
+        style={{
           display: "flex",
           justifyContent: "space-between",
           width: "200px",
-        },
-      },
-      React.createElement(
-        "button",
-        { onClick: handleClickYes, style: { marginRight: "10px" } },
-        "Yes"
-      ),
-      React.createElement(
-        "button",
-        { onClick: handleClickNo, style: { marginLeft: "10px" } },
-        "No"
-      )
-    )
+        }}
+      >
+        <button onClick={handleClickYes} style={{ marginRight: "10px" }}>
+          Yes
+        </button>
+        <button onClick={handleClickNo} style={{ marginLeft: "10px" }}>
+          No
+        </button>
+      </div>
+    </div>
   );
 };
 
 const App = () => {
-  return React.createElement(Pet);
+  return <Pet />;
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
