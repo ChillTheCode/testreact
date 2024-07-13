191e540bb07198809c93b7b68c1bdf8f16bf7ad2
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 01:15:40 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
-const App = () => {
+const Pet = () => {
+  const handleClickYes = () => {
+    alert("haloo ara");
+  };
+
+  const handleClickNo = () => {
+    alert("Give this link to ara plz :)");
+  };
+
   return (
-    <div style={{ textAlign: "center", marginTop: "20vh" }}>
-      <h1>Hello, world!</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div style={{ marginBottom: "20px" }}>
+        <img
+          src="./testcat.png"
+          alt="cat"
+          width="400"
+          style={{ alignSelf: "center" }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "200px",
+        }}
+      >
+        <button onClick={handleClickYes} style={{ marginRight: "10px" }}>
+          Yes
+        </button>
+        <button onClick={handleClickNo} style={{ marginLeft: "10px" }}>
+          No
+        </button>
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = () => {
+  return <Pet />;
+};
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
