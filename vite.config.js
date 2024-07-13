b86232f9dff0949a476f17137ea82929d357340c
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 02:22:30 +0700

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "src",
+  base: "/https://chillthecode.github.io/testreact/src/index.html/", // Replace with your project name or custom base URL
+  build: {
+    outDir: "dist",
+    assetsDir: "",
+    sourcemap: false,
+  },
 });
