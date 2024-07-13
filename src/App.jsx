b69881998d3475da6c3deb8753a701c9e75eb850
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 00:59:35 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,49 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom";
 
-const Pet = () => {
-  const handleClickYes = () => {
-    alert("haloo ara");
-  };
-
-  const handleClickNo = () => {
-    alert("Give this link to ara plz :)");
-  };
-
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center", // Memusatkan secara vertical dan horizontal
-        minHeight: "100vh", // Mengisi seluruh tinggi viewport
-      }}
-    >
-      <div style={{ marginBottom: "20px" }}>
-        <img
-          src="./testcat.png"
-          alt="cat"
-          width="400"
-          style={{ alignSelf: "center" }} // Memusatkan gambar secara horizontal
-        />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "200px",
-        }}
-      >
-        <button onClick={handleClickYes} style={{ marginRight: "10px" }}>
-          Yes
-        </button>
-        <button onClick={handleClickNo} style={{ marginLeft: "10px" }}>
-          No
-        </button>
-      </div>
-    </div>
+const Pet = (props) => {
+  return React.createElement(
+    "div",
+    {
+      style: { display: "flex", flexDirection: "column", alignItems: "center" },
+    },
+    [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h1", {}, props.animal),
+      React.createElement("h1", {}, props.breed),
+      React.createElement("img", {
+        src: props.image,
+        alt: props.name,
+        width: "200",
+        align: "center",
+      }),
+    ]
   );
 };
-
 const App = () => {
-  return <Pet />;
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Anjing"),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "luna",
+      breed: "Havanese",
+      image: "./family_guy.jpg",
+    }),
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "luna",
+      breed: "Havanese",
+      image: "./family_guy.jpg",
+    }),
+
+    React.createElement(Pet, {
+      animal: "Dog",
+      name: "luna",
+      breed: "Havanese",
+      image: false,
+    }),
+  ]);
 };
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(React.createElement(App));
