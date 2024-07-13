265b963c78179ea5d89e14c7e63479ae462deab5
ChillTheCode <edwin.daniel@ui.ac.id>
2024-07-14 01:09:42 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 
-const Pet = () => {
-  const handleClickYes = () => {
-    alert("haloo ara");
-  };
-
-  const handleClickNo = () => {
-    alert("Give this link to ara plz :)");
-  };
-
+const App = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center", // Memusatkan secara vertical dan horizontal
-        minHeight: "100vh", // Mengisi seluruh tinggi viewport
-      }}
-    >
-      <div style={{ marginBottom: "20px" }}>
-        <h1>Hello, world!</h1> {/* Teks yang ditampilkan */}
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "200px",
-        }}
-      >
-        <button onClick={handleClickYes} style={{ marginRight: "10px" }}>
-          Yes
-        </button>
-        <button onClick={handleClickNo} style={{ marginLeft: "10px" }}>
-          No
-        </button>
-      </div>
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>Hello, world!</h1>
     </div>
   );
 };
 
-const App = () => {
-  return <Pet />;
-};
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
